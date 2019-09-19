4858f0e7ab44d2d2b51f72df51b6022a488c0d4d
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-19 09:57:02 -0700

    solved save method

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -6,7 +6,23 @@ jest.mock('../lib/files.js', () => ({
 
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
+const DocumentCollection = require('../lib/document-collection');
+const documentNew = new DocumentCollection;
 
 describe('Document Collection', () => {
-  // TODO
+  it('saves a file', () => {
+    // arrange
+    const exampleObject = { name: 'Alex' };
+    const dest = './id.json';
+    writeFile.mockResolvedValue(exampleObject);
+    
+    // act
+    return documentNew.save(exampleObject)
+      .then(() => {
+        const writeCalls = writeFile.mock.calls;
+        expect(writeCalls.length).toBe(1);
+        expect(writeCalls[0][0]).toBe(dest);
+        expect(writeCalls[0][1]).toBe(JSON.stringify(exampleObject));
+      });
+  });
 });
