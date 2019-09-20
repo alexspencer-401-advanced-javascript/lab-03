b99cd8a933b15eef3f8df4756c2bed0198373533
Alex Spencer <alexander.j.spencer@gmail.com>
2019-09-19 17:29:58 -0700

    yeah

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -55,16 +55,23 @@ describe('Document Collection', () => {
   });
   it('gets all files from within a directory', () => {
     // arrange
-    const source = `./document/${exampleObject.id}.json`;
-    readFile.mockResolvedValue(exampleObject);
+    const exampleObject2 = { name: 'Alex', id: '123' };
+    
+    let returnedObject = Promise.resolve(JSON.stringify(exampleObject2));
+    readFile.mockResolvedValue(returnedObject);
+
+    let returnedDirPromise = Promise.resolve(['123.json']);
+    readdir.mockResolvedValue(returnedDirPromise);
+
+    const documentCollect = new DocumentCollection('document');
     
     // act
-    return documentNew.get(exampleObject.id)
-      .then(() => {
+    return documentCollect.getAll()
+      .then(array => {
         // const dest = `./${exampleObject.id}.json`;
-        const readCalls = readFile.mock.calls;
-        expect(readCalls.length).toBe(1); 
-        expect(readCalls[0][0]).toBe(source);
+        expect(readdir.mock.calls[0][0]).toBe('document');
+        expect(readFile.mock.calls[1][0]).toBe('./document/123.json');
+        expect(array[0].id).toBe(exampleObject2.id);
       });
   });
 });
